8dac369ac8dc3b380f809633ecc7763e2b3aba72
vagrant <vagrant@localhost.localdomain>
2020-03-27 07:58:04 +0000

    [finish]3-4

--- sample_app/db/migrate/20200327011206_create_lists.rb
+++ sample_app/db/migrate/20200327011206_create_lists.rb
@@ -3,7 +3,7 @@ class CreateLists < ActiveRecord::Migration[5.2]
     create_table :lists do |t|
     	t.string :title
         t.string :body
-       t.timestamps
+         t.timestamps
     end
   end
 end
