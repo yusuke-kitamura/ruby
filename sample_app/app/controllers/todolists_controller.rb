648154c1e717d064b32b8cc872bf6656ba188d78
vagrant <vagrant@localhost.localdomain>
2020-03-28 07:40:00 +0000

    [finish]3-5

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -2,4 +2,19 @@ class TodolistsController < ApplicationController
   def new
   	@list = List.new
   end
+
+  # 以下を追加
+  def create
+        # ストロングパラメーターを使用
+         list = List.new(list_params)
+        # DBへ保存する
+         list.save
+        # トップ画面へリダイレクト
+        redirect_to '/top'
+    end
+    private
+   
+    def list_params
+        params.require(:list).permit(:title, :body)
+    end
 end
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'todolists/new'
 	get 'top' => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+   post 'todolists' => 'todolists#create'
 end
